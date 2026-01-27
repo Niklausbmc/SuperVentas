7492e0d701ecbaba2a99bc180dca0c90c6aa86f8
Niklausbmc <bmartinezcalcerrada@gmail.com>
2026-01-27 01:17:08 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,10 +1,51 @@
-function login() {
-  const usuario = document.getElementById("usuario").value;
-  const clave = document.getElementById("clave").value;
+let productos = JSON.parse(localStorage.getItem("productos")) || [];
 
-  if (usuario === "admin" && clave === "1234") {
-    window.location.href = "panel.html";
-  } else {
-    alert("Usuario o contraseña incorrectos");
-  }
+function guardarProductos() {
+  localStorage.setItem("productos", JSON.stringify(productos));
 }
+
+function mostrarProductos() {
+  const lista = document.getElementById("listaProductos");
+  lista.innerHTML = "";
+
+  productos.forEach((p, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${p.nombre} - $${p.precio}
+      <button onclick="eliminarProducto(${index})">❌</button>
+    `;
+    lista.appendChild(li);
+  });
+}
+
+function agregarProducto() {
+  const nombre = document.getElementById("nombre").value;
+  const precio = document.getElementById("precio").value;
+  const imagen = document.getElementById("imagen").value;
+
+  if (nombre === "" || precio === "") {
+    alert("Completa los datos");
+    return;
+  }
+
+  productos.push({
+    nombre,
+    precio,
+    imagen
+  });
+
+  guardarProductos();
+  mostrarProductos();
+
+  document.getElementById("nombre").value = "";
+  document.getElementById("precio").value = "";
+  document.getElementById("imagen").value = "";
+}
+
+function eliminarProducto(index) {
+  productos.splice(index, 1);
+  guardarProductos();
+  mostrarProductos();
+}
+
+mostrarProductos();
