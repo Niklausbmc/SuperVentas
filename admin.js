268f7c9795abbb4a656f87a769d1341f2d2ff41d
Niklausbmc <bmartinezcalcerrada@gmail.com>
2026-01-28 13:17:03 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
   getFirestore,
   collection,
   addDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔥 MISMA CONFIG DE TU TIENDA
 const firebaseConfig = {
   apiKey: "AIzaSyBm8gyyEp04P7GAWyhP3XP_MUcD9ggNASH0",
   authDomain: "superventas-d50e2.firebaseapp.com",
@@ -16,18 +22,40 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-// 👇 HACER GLOBAL
-window.agregarProducto = async function () {
+// 👉 LOGIN
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  signInWithEmailAndPassword(auth, email, password)
+    .catch(err => alert("❌ Datos incorrectos"));
+};
+
+// 👉 VERIFICAR SESIÓN
+onAuthStateChanged(auth, user => {
+  if (user) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("panel").style.display = "block";
+    document.getElementById("titulo").innerText = "Panel Administrador";
+  } else {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("panel").style.display = "none";
+    document.getElementById("titulo").innerText = "Login Administrador";
+  }
+});
+
+// 👉 AGREGAR PRODUCTOS
+window.agregarProducto = async function () {
   const nombre = document.getElementById("nombre").value;
   const precio = Number(document.getElementById("precio").value);
   const stock = Number(document.getElementById("stock").value);
   const imagen = document.getElementById("imagen").value;
 
   if (!nombre || !precio || !stock || !imagen) {
-    alert("Completa todos los campos");
+    alert("Completa todo");
     return;
   }
 
@@ -38,10 +66,15 @@ window.agregarProducto = async function () {
     imagen
   });
 
-  alert("✅ Producto agregado");
+  alert("✅ Producto guardado");
 
   document.getElementById("nombre").value = "";
   document.getElementById("precio").value = "";
   document.getElementById("stock").value = "";
   document.getElementById("imagen").value = "";
+};
+
+// 👉 LOGOUT
+window.logout = function () {
+  signOut(auth);
 };
